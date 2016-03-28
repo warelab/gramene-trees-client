d927f95c6b7815bf445a3ff5d9cde5147f728303
Joe Mulvaney <jmulvane@cshl.edu>
2016-03-28 12:17:19 -0400

    correctly use gramene client api 2.x

--- src/promise.js
+++ src/promise.js
@@ -19,7 +19,7 @@ module.exports = {
           rows: -1,
           subset: 'gramene',
           fl: ['_id', 'is_a', 'property_value', 'name', 'synonym', 'num_genes']};
-        client['Data access'].taxonomy(params).then(function(response) {
+        client['Data access'].taxonomy(params, function(response) {
           response.client = client;
           deferred.resolve(response);
         });
